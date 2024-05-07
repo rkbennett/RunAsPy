0c858fec93018862ad2f4fe00d978d5488cfb160
rkbennett <r.k.bennett@hotmail.com>
2024-05-07 07:52:20 -0500

    makes username, password, and command args required in argparse

--- RunAs.py
+++ RunAs.py
@@ -3,9 +3,9 @@ import RunAsPy
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument('-d', '--domain', help="", nargs="?", dest="domainName")
-parser.add_argument('-u', '--username', help="", nargs="?")
-parser.add_argument('-P', '--password', help="", nargs="?")
-parser.add_argument('-c', '--command', help="", nargs="*", dest="cmd")
+parser.add_argument('-u', '--username', help="", nargs="?", required=True)
+parser.add_argument('-P', '--password', help="", nargs="?", required=True)
+parser.add_argument('-c', '--command', help="", nargs="*", dest="cmd", required=True)
 parser.add_argument('-t', '--timeout', help="", nargs="?", default=120000, dest="processTimeout", type=int)
 parser.add_argument('-l', '--logon-type', help="", nargs="?", default=2, dest="logonType", type=int, choices=[2, 3, 4, 5, 8, 9])
 parser.add_argument('-f', '--function', help="", nargs="?", dest="createProcessFunction", default=RunAsPy.DefaultCreateProcessFunction(), type=int)
